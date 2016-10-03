0ff68a99d8c6e042138f5edd6ca67d2fdfa99e72
Christian Curry <christian.curry17@gmail.com>
2016-10-02 23:43:47 -0400

    added database methods to search by vehicle attributes (some can be null, or none), edit a specific vehicle, or delete a vehicle.

--- UsedCarSales/Database.cs
+++ UsedCarSales/Database.cs
@@ -75,41 +75,136 @@ namespace UsedCarSales
             command.Parameters.AddWithValue("@sold", vehicle.sold);
             command.Parameters.AddWithValue("@model", vehicle.model);
             command.ExecuteNonQuery();
+
+            //TODO: print vehicle information for debugging
+            Console.WriteLine("New vehicle added succesfully");
         }
 
-        public void test()
+        public void editVehicle(Vehicle vehicle)
         {
-            var query = "SELECT * FROM Vehicle";
+            var query = "UPDATE Vehicle SET used=@used, sold=@sold, model=@model WHERE id=@id";
+
             MySqlCommand command = new MySqlCommand(query, connection);
-            MySqlDataReader reader = command.ExecuteReader();
 
-            Vehicle vehicle;
-            List<Vehicle> vehicles = new List<Vehicle>();
-            while (reader.Read())
+            //need the ID to edit the specific vehicle
+            command.Parameters.AddWithValue("@id", vehicle.id);
+
+            command.Parameters.AddWithValue("@used", vehicle.used);
+            command.Parameters.AddWithValue("@sold", vehicle.sold);
+            command.Parameters.AddWithValue("@model", vehicle.model);
+            command.ExecuteNonQuery();
+
+            Console.WriteLine("Vehicle {0} edited successfully", vehicle.id);
+        }
+
+        public void deleteVehicle(Vehicle vehicle)
+        {
+            var query = "DELETE FROM Vehicle WHERE id=@id";
+
+            MySqlCommand command = new MySqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@id", vehicle.id);
+
+            Console.WriteLine("Vehicle {0} deleted successfully", vehicle.id);
+        }
+
+        public List<Vehicle> searchVehicle(Vehicle vehicle)
+        {
+            var query = "SELECT * FROM Vehicle WHERE ";
+
+            //TODO: write a function to make this simpler. just trying to get it working before tuesday
+            int count = 0;
+            if (vehicle.used != null)
             {
-                vehicle = new Vehicle();
+                //add used to the query
+                query += "used=@used";
+                count++;
+            }
+            if (vehicle.sold != null)
+            {
+                //if we already have a WHERE clause, add an AND clause
+                if (count > 0)
+                {
+                    query += " AND ";
+                }
 
-                vehicle.id = int.Parse(reader["id"].ToString());
-                vehicle.used = bool.Parse(reader["used"].ToString());
-                vehicle.sold = bool.Parse(reader["sold"].ToString());
-                vehicle.model = int.Parse(reader["model"].ToString());
+                query += "sold=@sold";
+                count++;
+            }
+            if (vehicle.model != null)
+            {
+                //if we already have a WHERE clause, add an AND clause
+                if (count > 0)
+                {
+                    query += " AND ";
+                }
 
-                vehicles.Add(vehicle);
+                query += "model=@model";
+                //count does not need to be incremented here unless we add another variable to search by
             }
 
-            foreach (Vehicle v in vehicles)
+            MySqlCommand command = new MySqlCommand(query, connection);
+
+            if (vehicle.used != null)
             {
-                Console.WriteLine("Id: {0}, Used: {1}, Sold: {2}, Model: {3}", v.id, v.used, v.sold, v.model);
+                command.Parameters.AddWithValue("@used", vehicle.used);
+            }
+            if (vehicle.sold != null)
+            {
+                command.Parameters.AddWithValue("@sold", vehicle.sold);
+            }
+            if (vehicle.model != null)
+            {
+                command.Parameters.AddWithValue("@model", vehicle.model);
+            }
+
+            MySqlDataReader reader = command.ExecuteReader();
+
+            //TODO: need a function for this, will probably be reused
+            List<Vehicle> vehicles = new List<Vehicle>();
+
+            Vehicle v;
+            while (reader.Read())
+            {
+                v = new Vehicle();
+
+                v.id = Int32.Parse(reader["id"].ToString());
+                v.used = Boolean.Parse(reader["used"].ToString());
+                v.sold = Boolean.Parse(reader["sold"].ToString());
+                v.model = Int32.Parse(reader["model"].ToString());
+
+                vehicles.Add(v);
             }
 
             reader.Close();
 
-            Vehicle newVehicle = new Vehicle();
-            newVehicle.used = false;
-            newVehicle.sold = false;
-            newVehicle.model = 2;
+            Console.WriteLine("Vehicles retrieved successfully");
 
-            addVehicle(newVehicle);
+            return vehicles;
+        }
+
+        public void test()
+        {
+            //TODO: i have not actually completely confirmed that any of this works correctly yet
+
+            Vehicle vehicle = new Vehicle();
+            vehicle.sold = false;
+            vehicle.used = true;
+            vehicle.model = 2;
+
+            addVehicle(vehicle);
+
+            vehicle.sold = true;
+            vehicle.used = null;
+            editVehicle(vehicle);
+
+            List<Vehicle> vehicles = searchVehicle(vehicle);
+            foreach(Vehicle v in vehicles)
+            {
+                Console.WriteLine("Id: {0}, Sold: {1}, Used: {2}, Model: {3}", v.id, v.sold, v.used, v.model);
+            }
+
+            deleteVehicle(vehicle);
         }
 
     }
--- UsedCarSales/Vehicle.cs
+++ UsedCarSales/Vehicle.cs
@@ -8,10 +8,11 @@ namespace UsedCarSales
 {
     class Vehicle
     {
+        //question marks mean that the variable can be set to null
         public int id { get; set; }
-        public bool used { get; set; }
-        public bool sold { get; set; }
-        public int model { get; set; }
+        public bool? used { get; set; }
+        public bool? sold { get; set; }
+        public int? model { get; set; }
 
         
 
